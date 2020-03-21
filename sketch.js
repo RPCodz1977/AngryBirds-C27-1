1d506a3a4ca3853dc05be7606ca64a4bec55f75c
RPCodz1977 <rpkillz.fortnite@gmail.com>
2020-03-21 20:09:06 +0530

    Constraint added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform1;
+var chain;
+var log1,log2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,8 +17,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
-
     ground = new Ground(600,height,1200,20);
     platform1 = new Ground(150, 305, 300, 170);
 
@@ -33,9 +34,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230, 180, 80, PI/2);
     bird = new Bird(100,100);
-
+chain = new Chain(bird.body, log6.body);
 }
 
 function draw(){
@@ -61,4 +62,9 @@ function draw(){
 
     bird.display();
     platform1.display();
+    log6.display();
+    chain.display();
+
+
+
 }
